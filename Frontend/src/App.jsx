b923b53d5bb70c67fcc8d0021ea0dd810f0cab80
rohitdhadambe <rohitdhadambe@gmail.com>
2024-10-11 13:57:50 +0530

    api handling

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,40 @@
+
+import { useEffect, useState } from 'react'
+import './index.css'
+import axios from 'axios'
+
+function App() {
+
+const [products,setproducts] = useState([]);
+const [loading,setloading] = useState();
+
+useEffect(()=>{
+;(async()=>{
+      try {
+        setloading(true)
+         const resp =await axios.get('/api/products')
+         console.log(resp.data);
+         setproducts(resp.data);
+         setloading(false)
+      } catch (error) {
+        setloading(false)
+        error
+      }
+})()
+
+},[])
+
+if(loading){
+ return <h1>LOADING....!</h1>
+}
+
+  return (
+    <>
+      <h1>API HANDLING</h1>
+      <h1>NO OF PRODUCTS : {products.length}</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+const app = express()
+
+app.get('/api/products', (req,res)=>{
+    const products = [
+        {
+          id: 101,
+          name: "Watch",
+          price: 200
+        },
+        {
+          id: 102,
+          name: "Phone",
+          price: 500
+        },
+        {
+          id: 103,
+          name: "Laptop",
+          price: 1000
+        },
+        {
+          id: 104,
+          name: "Headphones",
+          price: 150
+        },
+        {
+          id: 105,
+          name: "Tablet",
+          price: 300
+        }
+      ];
+      
+  setTimeout(()=>{
+    res.send(products)
+  },2000)
+
+//http://localhost:3000/api/products?search=Watch
+//to Search any product
+// we need to write some code
+
+})
+
+
+app.listen(3000,()=>{
+    console.log("server is running");
+});
